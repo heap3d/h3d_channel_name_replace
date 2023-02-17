8860d88b7eec800136f2fcb17a8c14774cccbc7e
Dmytro Holub <heap3d@gmail.com>
2023-02-17 16:32:05 +0200

    rename all selected channels

--- scripts/channel_name_replace.py
+++ scripts/channel_name_replace.py
@@ -5,7 +5,7 @@
 # --------------------------------
 # modo python
 # EMAG
-# search and replace user channel names for selected item
+# search and replace selected user channel names
 
 
 import modo
@@ -19,20 +19,37 @@ from h3d_debug import H3dDebug
 from h3d_exceptions import H3dExitException
 
 
+USERVAL_CNR_SEARCH_NAME = 'h3d_cnr_search'
+USERVAL_CNR_REPLACE_NAME = 'h3d_cnr_replace'
+
+
 def main():
-    items = modo.Scene().selected
+    SEARCH_STR = h3du.get_user_value(USERVAL_CNR_SEARCH_NAME)
+    REPLACE_STR = h3du.get_user_value(USERVAL_CNR_REPLACE_NAME)
 
-    SEARCH_STR = ''
-    REPLACE_STR = ''
+    selection_service = lx.service.Selection()
+    chan_sel_type = selection_service.LookupType(lx.symbol.sSELTYP_CHANNEL)
 
-    for item in items:
-        item.select(replace=True)
-        print(item.name)
-        for channel in item.channels():
-            lx.eval("select.drop channel")
-            lx.eval("select.channel {{{}:{}@lmb=x}} add".format(item.id, channel.name))
-            newname = '{}'.format(channel.name).replace(SEARCH_STR, REPLACE_STR)
-            lx.eval('channel.username username:"{}"'.format(newname))
+    chan_transpacket = lx.object.ChannelPacketTranslation(selection_service.Allocate(lx.symbol.sSELTYP_CHANNEL))
+
+    chan_n = selection_service.Count(chan_sel_type)
+
+    for x in range(chan_n):
+        packet_pointer = selection_service.ByIndex(chan_sel_type, x)
+        if not packet_pointer:
+            lx.out('Bad selection packet, skipping...')
+            continue
+
+        item = lx.object.Item(chan_transpacket.Item(packet_pointer))
+        chan_idx = chan_transpacket.Index(packet_pointer)
+        # item_id = item.UniqueName()
+        item_id = item.Ident()
+        channel_id = item.ChannelName(chan_idx)
+        # lx.out('%s : %s' % (item_id, channel_id))
+        username = lx.eval('channel.username channel:{{{}:{}}} username:?'.format(item_id, channel_id))
+        new_username = '{}'.format(username).replace(SEARCH_STR, REPLACE_STR)
+        # print(username, new_username)
+        lx.eval('channel.username channel:{{{}:{}}} username:{{{}}}'.format(item_id, channel_id, new_username))
 
 
 if __name__ == '__main__':
